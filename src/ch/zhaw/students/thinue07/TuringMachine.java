58e41423bae5f4a8ca8c2566f998619e7bfe878b
Philippe Hürlimann <p@hurlimann.org>
2016-11-30 16:44:49 +0100

    ZERO -> ONE and ONE -> ZERO

--- src/ch/zhaw/students/thinue07/TuringMachine.java
+++ src/ch/zhaw/students/thinue07/TuringMachine.java
@@ -97,39 +97,41 @@ public class TuringMachine {
 	}
 
 	private final static Map<DeltaInput, DeltaNew> Transitions = new HashMap<DeltaInput, DeltaNew>() {{
-		put(new DeltaInput(State.Q0, Alphabet.ONE), new DeltaNew(State.Q1, Alphabet.X, Movement.RIGHT));
-		put(new DeltaInput(State.Q1, Alphabet.ONE), new DeltaNew(State.Q1, Alphabet.ONE, Movement.RIGHT));
-		put(new DeltaInput(State.Q1, Alphabet.ZERO), new DeltaNew(State.Q2, Alphabet.ZERO, Movement.RIGHT));
+		put(new DeltaInput(State.Q0, Alphabet.ZERO), new DeltaNew(State.Q1, Alphabet.X, Movement.RIGHT));
 
-		put(new DeltaInput(State.Q2, Alphabet.ONE), new DeltaNew(State.Q3, Alphabet.Y, Movement.RIGHT));
+		put(new DeltaInput(State.Q1, Alphabet.ONE), new DeltaNew(State.Q2, Alphabet.ONE, Movement.RIGHT));
+		put(new DeltaInput(State.Q1, Alphabet.ZERO), new DeltaNew(State.Q1, Alphabet.ZERO, Movement.RIGHT));
+
+		put(new DeltaInput(State.Q2, Alphabet.ZERO), new DeltaNew(State.Q3, Alphabet.Y, Movement.RIGHT));
 
 		put(new DeltaInput(State.Q3, Alphabet.BLANK), new DeltaNew(State.Q4, Alphabet.Z, Movement.RIGHT));
-		put(new DeltaInput(State.Q3, Alphabet.ONE), new DeltaNew(State.Q3, Alphabet.ONE, Movement.RIGHT));
 		put(new DeltaInput(State.Q3, Alphabet.Z), new DeltaNew(State.Q4, Alphabet.Z, Movement.RIGHT));
+		put(new DeltaInput(State.Q3, Alphabet.ZERO), new DeltaNew(State.Q3, Alphabet.ZERO, Movement.RIGHT));
 
-		put(new DeltaInput(State.Q4, Alphabet.BLANK), new DeltaNew(State.Q5, Alphabet.ONE, Movement.LEFT));
-		put(new DeltaInput(State.Q4, Alphabet.ONE), new DeltaNew(State.Q4, Alphabet.ONE, Movement.RIGHT));
+		put(new DeltaInput(State.Q4, Alphabet.BLANK), new DeltaNew(State.Q5, Alphabet.ZERO, Movement.LEFT));
+		put(new DeltaInput(State.Q4, Alphabet.ZERO), new DeltaNew(State.Q4, Alphabet.ZERO, Movement.RIGHT));
 
-		put(new DeltaInput(State.Q5, Alphabet.ONE), new DeltaNew(State.Q5, Alphabet.ONE, Movement.LEFT));
 		put(new DeltaInput(State.Q5, Alphabet.Y), new DeltaNew(State.Q6, Alphabet.Y, Movement.RIGHT));
 		put(new DeltaInput(State.Q5, Alphabet.Z), new DeltaNew(State.Q5, Alphabet.Z, Movement.LEFT));
+		put(new DeltaInput(State.Q5, Alphabet.ZERO), new DeltaNew(State.Q5, Alphabet.ZERO, Movement.LEFT));
 
-		put(new DeltaInput(State.Q6, Alphabet.ONE), new DeltaNew(State.Q7, Alphabet.Y, Movement.RIGHT));
+		put(new DeltaInput(State.Q6, Alphabet.ZERO), new DeltaNew(State.Q7, Alphabet.Y, Movement.RIGHT));
 
-		put(new DeltaInput(State.Q7, Alphabet.BLANK), new DeltaNew(State.Q8, Alphabet.ONE, Movement.RIGHT));
-		put(new DeltaInput(State.Q7, Alphabet.ONE), new DeltaNew(State.Q7, Alphabet.ONE, Movement.RIGHT));
+		put(new DeltaInput(State.Q7, Alphabet.BLANK), new DeltaNew(State.Q8, Alphabet.ZERO, Movement.RIGHT));
 		put(new DeltaInput(State.Q7, Alphabet.Z), new DeltaNew(State.Q7, Alphabet.Z, Movement.RIGHT));
+		put(new DeltaInput(State.Q7, Alphabet.ZERO), new DeltaNew(State.Q7, Alphabet.ZERO, Movement.RIGHT));
 
 		put(new DeltaInput(State.Q8, Alphabet.BLANK), new DeltaNew(State.Q9, Alphabet.BLANK, Movement.LEFT));
-		put(new DeltaInput(State.Q9, Alphabet.ONE), new DeltaNew(State.Q9, Alphabet.ONE, Movement.LEFT));
+
 		put(new DeltaInput(State.Q9, Alphabet.Z), new DeltaNew(State.Q10, Alphabet.Z, Movement.LEFT));
+		put(new DeltaInput(State.Q9, Alphabet.ZERO), new DeltaNew(State.Q9, Alphabet.ZERO, Movement.LEFT));
 
-		put(new DeltaInput(State.Q10, Alphabet.ONE), new DeltaNew(State.Q5, Alphabet.ONE, Movement.LEFT));
-		put(new DeltaInput(State.Q10, Alphabet.Y), new DeltaNew(State.Q10, Alphabet.ONE, Movement.LEFT));
-		put(new DeltaInput(State.Q10, Alphabet.ZERO), new DeltaNew(State.Q11, Alphabet.ZERO, Movement.LEFT));
+		put(new DeltaInput(State.Q10, Alphabet.ONE), new DeltaNew(State.Q11, Alphabet.ONE, Movement.LEFT));
+		put(new DeltaInput(State.Q10, Alphabet.Y), new DeltaNew(State.Q10, Alphabet.ZERO, Movement.LEFT));
+		put(new DeltaInput(State.Q10, Alphabet.ZERO), new DeltaNew(State.Q5, Alphabet.ZERO, Movement.LEFT));
 
-		put(new DeltaInput(State.Q11, Alphabet.ONE), new DeltaNew(State.Q15, Alphabet.ONE, Movement.LEFT));
 		put(new DeltaInput(State.Q11, Alphabet.X), new DeltaNew(State.Q12, Alphabet.X, Movement.LEFT));
+		put(new DeltaInput(State.Q11, Alphabet.ZERO), new DeltaNew(State.Q15, Alphabet.ZERO, Movement.LEFT));
 
 		put(new DeltaInput(State.Q12, Alphabet.BLANK), new DeltaNew(State.Q13, Alphabet.BLANK, Movement.RIGHT));
 		put(new DeltaInput(State.Q12, Alphabet.X), new DeltaNew(State.Q12, Alphabet.X, Movement.LEFT));
@@ -139,8 +141,8 @@ public class TuringMachine {
 		put(new DeltaInput(State.Q13, Alphabet.Z), new DeltaNew(State.Q14, Alphabet.BLANK, Movement.RIGHT));
 		put(new DeltaInput(State.Q13, Alphabet.ZERO), new DeltaNew(State.Q13, Alphabet.BLANK, Movement.RIGHT));
 
-		put(new DeltaInput(State.Q15, Alphabet.ONE), new DeltaNew(State.Q15, Alphabet.ONE, Movement.LEFT));
 		put(new DeltaInput(State.Q15, Alphabet.X), new DeltaNew(State.Q0, Alphabet.X, Movement.RIGHT));
+		put(new DeltaInput(State.Q15, Alphabet.ZERO), new DeltaNew(State.Q15, Alphabet.ZERO, Movement.LEFT));
 	}};
 
 	private static final State INITIAL_STATE = State.Q0;
