e0fca1cfc68be1f0af38f9ba2cfde9b9301b533d
Philippe Hürlimann <p@hurlimann.org>
2016-11-30 14:42:26 +0100

    Implemented input parsing

--- src/ch/zhaw/students/thinue07/Main.java
+++ src/ch/zhaw/students/thinue07/Main.java
@@ -1,9 +1,18 @@
 package ch.zhaw.students.thinue07;
 
+import java.util.Optional;
+import java.util.stream.Stream;
+
 public class Main {
 
 	public static void main(String[] args) {
-		TuringMachine myMachine = new TuringMachine("11011", true);
-		myMachine.Run();
+		boolean verbose = Stream.of(args).anyMatch("-v"::equals);
+		Optional<String> input = Stream.of(args).filter(a -> a.matches("[01]+")).findFirst();
+		if (input.isPresent()) {
+			TuringMachine myMachine = new TuringMachine(input.get(), verbose);
+			myMachine.Run();
+		} else {
+			System.out.println("Please enter a valid input string");
+		}
 	}
 }
